c42773bbf5ac8a5da65519b1351a559e9cf3a5a1
Abraham Trashorras <abraham.cocodin@gmail.com>
2021-02-09 17:03:38 +0100

    First commit

---
+++ src/main/java/com/example/enser_p1/EnserP1Application.java
@@ -0,0 +1,13 @@
+package com.example.enser_p1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnserP1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnserP1Application.class, args);
+    }
+
+}
